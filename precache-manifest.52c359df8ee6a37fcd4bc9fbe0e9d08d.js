3f62a3d030612cb57241d542d000d3c4e4dcc74d
nitingupta220 <25584421+nitingupta220@users.noreply.github.com>
2020-07-07 05:55:43 +0000

    Deploying to gh-pages from  @ 8f188b4ffec055d4ebef8e19be91f3b9fb4ad762 🚀

---
+++ precache-manifest.52c359df8ee6a37fcd4bc9fbe0e9d08d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2972cc074b0d3aa5275b2e9decafe6",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "56ad57904c5496c39f6e",
+    "url": "/covid19-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9fd3747c2f76e1f6af34",
+    "url": "/covid19-tracker/static/js/2.1fb9b742.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19-tracker/static/js/2.1fb9b742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ad57904c5496c39f6e",
+    "url": "/covid19-tracker/static/js/main.50e2613c.chunk.js"
+  },
+  {
+    "revision": "8cd54aa8f9c07f9dab41",
+    "url": "/covid19-tracker/static/js/runtime-main.c8a8ed36.js"
+  }
+]);
